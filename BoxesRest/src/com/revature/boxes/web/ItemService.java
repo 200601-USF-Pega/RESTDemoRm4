bc241bcdb5fc04f0507933c2863c1cfaf7ef6de5
moriaritydevin <moriaritydevin@yahoo.com>
2020-06-23 15:59:36 -0400

    add itemservice implementation

--- BoxesRest/src/com/revature/boxes/web/ItemService.java
+++ BoxesRest/src/com/revature/boxes/web/ItemService.java
@@ -1,10 +1,42 @@
 package com.revature.boxes.web;
 
+import java.util.ArrayList;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.revature.boxes.models.Box;
 
 @Path("/itemservice")
 public class ItemService {
 
+	IItemRepo itemRepo = new ItemRepoDB();
 	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response addItem(Item item, Box box) {
+		itemRepo.addItem(item, box);
+		return Response.status(201).build();
+	}
 	
+	@GET
+	@Path("/allitems")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getAllItems() {
+		return Response.ok((ArrayList<Item>)itemRepo.getAllItems()).build();
+	}
+	
+	@DELETE
+	@Path("/removeitem")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response removeItem(Item item) {
+		itemRepo.removeItem(item);
+		return Response.status(201).build();
+	}
 }
