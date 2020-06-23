994c40ae4cafa998d57d9b831010faf0bef72b3e
Danny <danny.j.eich@gmail.com>
2020-06-23 15:28:46 -0400

    Updated DAO

--- BoxesRest/src/com/revature/boxes/dao/BoxRepoDB.java
+++ BoxesRest/src/com/revature/boxes/dao/BoxRepoDB.java
@@ -17,26 +17,19 @@ import com.revature.boxes.web.ConnectionService;
 public class BoxRepoDB implements IBoxRepo{
 
 	@Override
-	public Box addHero(Box hero) {
+	public Box addBox(Box box) {
 		// 1. Insert one row with hero's single properties
 		// 2. Insert one row per special move
 		
 		try {
 			
 			
-			PreparedStatement heroStatement = ConnectionService.getConnection().prepareStatement("INSERT INTO heroes VALUES (?, ?, ?)");
-			heroStatement.setString(1, hero.getName());
-			heroStatement.setInt(2, hero.getHealthLevel());
-			heroStatement.setInt(3, hero.isAlive()?1:0);
-			heroStatement.executeUpdate();
+			PreparedStatement boxStatement = ConnectionService.getConnection().prepareStatement("INSERT INTO boxes VALUES (?, ?)");
+			boxStatement.setString(1, box.getLocation());
+			boxStatement.setInt(2, box.getWeight());
+			boxStatement.executeUpdate();			
 			
-			for (String move : hero.getSpecialMove()) {
-				Statement moveStatement = ConnectionService.getConnection().createStatement();
-				moveStatement.executeUpdate("INSERT INTO herospecialmoves (heroName, specialMove) VALUES ('"
-						+ hero.getName() + "', '" + move + "');");
-			}
-			
-			return hero;
+			return box;
 			
 		} catch (SQLException e) {
 			System.out.println("Exception: " + e.getMessage());
@@ -48,40 +41,23 @@ public class BoxRepoDB implements IBoxRepo{
 	}
 
 	@Override
-	public List<Box> getAllHeros() {
+	public List<Box> getAllBoxes() {
 		
 		List<Box> result = new ArrayList<Box>();
-		Map<String, ArrayList<String>> specialMoves = new HashMap<String, ArrayList<String>>();
 		
 		try {
 			Statement s = ConnectionService.getConnection().createStatement();
-			s.executeQuery("SELECT h.*, hs.specialMove " + 
-					"FROM heroes AS h " + 
-					"LEFT JOIN herospecialmoves AS hs " + 
-					"ON h.name = hs.heroname;");
+			s.executeQuery("SELECT * FROM boxes;");
 			
 			ResultSet rs = s.getResultSet();
 			while (rs.next()) {
-				Box h = new Box();
-				h.setName(rs.getString("name"));
-				h.setHealthLevel(rs.getInt("healthLevel"));
-				h.setAlive(rs.getInt("isAlive") == 1);
-				if (!result.contains(h)) {
-					result.add(h);
+				Box tempBox = new Box();
+				tempBox.setLocation(rs.getString("boxLoc"));
+				tempBox.setWeight(rs.getDouble("boxWeight"));
+				if (!result.contains(tempBox)) {
+					result.add(tempBox);
 				}
 				
-				ArrayList<String> al = specialMoves.get(rs.getString("name"));
-				if (al == null) {
-					specialMoves.put(rs.getString("name"), new ArrayList<String>());
-				}
-				specialMoves.get(rs.getString("name")).add(rs.getString("specialmove"));
-			}
-			
-			for (Box hero : result) {
-				ArrayList<String> moves = specialMoves.get(hero.getName());
-				if (moves != null) {
-					hero.setSpecialMove(moves.toArray(new String[moves.size()]));
-				}
 			}
 			
 			return result;
---
+++ BoxesRest/src/com/revature/boxes/web/MyProp.java
@@ -0,0 +1,5 @@
+package com.revature.boxes.web;
+
+public class MyProp {
+
+}
